68b4cbc766f7312cbd16c4d6c261bd0edbf77391
WitherStormX <bussiness.reallycasper@gmail.com>
2026-06-27 21:04:08 +0530

    Create autoresponder.js

---
+++ src/commands/Moderation/autoresponder.js
@@ -0,0 +1,24 @@
+import { Events } from 'discord.js';
+
+const AUTO_RESPONSES = {
+    "texture pack": "<#1520081010056757318>"
+};
+
+export default {
+    name: Events.MessageCreate,
+
+    async execute(message) {
+        if (message.author.bot) return;
+
+        const content = message.content.toLowerCase();
+
+        for (const trigger in AUTO_RESPONSES) {
+            if (content.includes(trigger.toLowerCase())) {
+                await message.reply({
+                    content: AUTO_RESPONSES[trigger]
+                });
+                break;
+            }
+        }
+    }
+};
